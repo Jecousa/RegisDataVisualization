053d3478a0cc1fc8eb7ce2389087fd4ea34d1c20
Your Name <jecolousa@gmail.com>
2017-12-06 14:04:39 -0700

    Web Service

---
+++ index.php
@@ -0,0 +1,11 @@
+
+
+        <?php header('Access-Control-Allow-Origin: *');
+        echo '[
+       {"label": "Nikon", "size": 19},
+       {"label": "Canon", "size": 13},
+       {"label": "Sony", "size": 11},
+       {"label": "Olympus", "size": 7}
+         ]'
+        ?>
+    
